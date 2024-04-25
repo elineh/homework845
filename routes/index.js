46d9fcb25db15026149e63fa3803b7a6c2cc8f90
Eline H <eline.haukanes@scaleaq.com>
2024-04-25 08:16:44 +0200

    Updating to use s3 storage

--- routes/index.js
+++ routes/index.js
@@ -2,47 +2,62 @@ const { Router } = require("express");
 const { save } = require("../save_json");
 let favouriteNumber = require("../number.json");
 const add = require("../add");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
 const router = new Router();
 
-router.get("/sum/:number1/:number2", (req, res) => {
-  const {number1, number2} = req.params;
-  if(number1 == null || number2 == null) {
+// TODO - will fail due to bucket name being wrong
+router.get("/sum/:number1/:number2", async (req, res) => {
+  let my_file = await s3
+    .getObject({
+      Bucket: "cyclic-amused-bikini-bee-eu-north-1",
+      Key: "number.json"
+    })
+    .promise();
+
+  const favNumber = JSON.parse(my_file.Body)?.favouriteNumber;
+  const { number1, number2 } = req.params;
+  if (number1 == null || number2 == null) {
     res.status(400).send("Not provided numbers");
     return;
   }
-  if(isNaN(parseInt(number1)) || isNaN(parseInt(number2))) {
+  if (isNaN(parseInt(number1)) || isNaN(parseInt(number2))) {
     res.status(400).send("Numbers needs to be integer");
     return;
   }
-  const result = add(favouriteNumber.favouriteNumber, add(parseInt(number1), parseInt(number2)));
-  res.json({
-    status: "success",
-    result: result,
-  }); 
-});
-
-router.post("/favNumber", (req, res) => {
-  const {number} = req.body;
-  if(number == null ) {
-    res.status(400).send("Not provided number");
-    return;
-  } 
-  if(isNaN(parseInt(number))) {
-    res.status(400).send("The number needs to be integer");
-    return;
+  let result = add(parseInt(number1), parseInt(number2));
+  if (favNumber != null) {
+    result = add(result, favNumber);
   }
-  favouriteNumber.favouriteNumber = number;
-  save(favouriteNumber);
   res.json({
     status: "success",
-    newFavouriteNumber: number,
+    result: result
   });
 });
 
-router.delete("/favNumber", (req, res) => {
-  favouriteNumber.favouriteNumber = 0; 
-  save(favouriteNumber);
+router.post("/favNumber", async (req, res) => {
+  const { number } = req.body;
+  if (number == null) {
+    res.status(400).send("Not provided number");
+    return;
+  }
+  if (isNaN(parseInt(number))) {
+    res.status(400).send("The number needs to be integer");
+    return;
+  }
+  await save({ favouriteNumber: number });
+  res.json({
+    status: "success",
+    newFavouriteNumber: number
+  });
+});
+
+// Does this need a change as well?
+// TODO - this might fail
+router.delete("/favNumber", async (req, res) => {
+  favouriteNumber.favouriteNumber = 0;
+  await save(favouriteNumber);
   res.json({
     status: "success"
   });
--- save_json.js
+++ save_json.js
@@ -1,17 +1,19 @@
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
-const save = (favNumber) => {
+// Store something
+// TODO - will fail due to bucket name being wrong
+const save = async (favNumber) => {
   console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+  await s3
+    .putObject({
+      Body: JSON.stringify(favNumber, null, 2),
+      Bucket: "cyclic-amused-bikini-bee-eu-north-1",
+      Key: "number.json"
+    })
+    .promise();
 };
 
 module.exports = { save };
